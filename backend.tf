bb4f8f2bd83c7cc01bb0676b96637c00189d4f49
ethorneloe <ethorneloe@gmail.com>
2024-08-26 18:53:00 +1000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name  = "tfstatelab1"
     container_name        = "automation-account-testing"
     key                   = "terraform.tfstate"
+    use_azuread_auth      = true
   }
 }
