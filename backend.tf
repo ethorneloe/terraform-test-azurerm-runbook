21a74b459c9cfe81f9c64c1475506f67ab9823c5
ethorneloe <ethorneloe@gmail.com>
2024-09-11 16:12:41 +1000

    Testing

--- backend.tf
+++ backend.tf
@@ -1,6 +1,9 @@
- terraform {
-   backend "azurerm" {
-     use_oidc = true
-     use_azuread_auth = true
-   }
- }
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "tfstate"
+    storage_account_name  = "tfstatelab1"
+    container_name        = "automation-account-testing"
+    key                   = "terraform.tfstate"
+    use_azuread_auth      = true
+  }
+}
