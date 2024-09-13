58cf8b461a0c21f59a88ebf090727be7c5804a98
ethorneloe <ethorneloe@gmail.com>
2024-09-13 11:02:56 +1000

    Add tfvars support

--- non-production.tfvars
+++
@@ -1,4 +0,0 @@
-schedules_enabled_global = false
-schedules_enabled_overrides = {
-  "Runbook1-Daily" = true
-}
--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "automation_schedule_timezone" {
   description = "The timezone for schedules"
   default     = "Australia/Brisbane"
 }
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group."
+}
+
+variable "automation_account_name" {
+  type        = string
+  description = "The name of the automation account."
+}
