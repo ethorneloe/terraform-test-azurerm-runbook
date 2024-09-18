b1f7f26b8d88dac159bd2201d6ce009a01364543
Eric Thorneloe <129253602+ethorneloe@users.noreply.github.com>
2024-09-18 13:18:50 +1000

    New runbook (#10)

---
+++ Test-ExampleRunbook4.tf
@@ -0,0 +1,80 @@
+module "Runbook4" {
+  source = "git::https://github.com/ethorneloe/terraform-azurerm-automation-runbook.git?ref=v2.0.0"
+
+  resource_group_name = data.azurerm_resource_group.existing.name
+  automation_account_name = data.azurerm_automation_account.existing.name
+  location = var.location
+  timezone = var.automation_schedule_timezone
+
+  runbook = {
+    name         = "Test-ExampleRunbook4"
+    description  = "First example runbook"
+    content      = file("./azure-runbooks/Test-ExampleRunbook4.ps1")
+    log_verbose  = true
+    log_progress = true
+    runbook_type = "PowerShell72"
+  }
+
+  schedules = [
+    {
+      name        = "Runbook4-Daily1"
+      frequency   = "Day"
+      interval    = 1
+      start_time  = "2024-09-19T01:00:00+10:00"
+      description = "Runbook4-Daily1"
+      enabled     = false
+      run_on      = ""
+    },
+    {
+      name        = "Runbook4-Daily2"
+      frequency   = "Day"
+      interval    = 1
+      start_time  = "2024-09-19T01:00:00+10:00"
+      description = "Runbook4-Daily2"
+      enabled     = false
+      run_on      = ""
+    },
+    {
+      name        = "Runbook4-Weekly1"
+      frequency   = "Week"
+      interval    = 1
+      start_time  = "2024-09-19T01:00:00+10:00"
+      description = "Runbook4-Weekly1"
+      enabled     = true
+      run_on      = ""
+    },
+    {
+      name        = "Runbook4-Weekly2"
+      frequency   = "Week"
+      interval    = 1
+      start_time  = "2024-09-19T01:00:00+10:00"
+      description = "Runbook4-Weekly2"
+      week_days   = ["Monday", "Friday"]
+      run_on      = ""
+    },
+    {
+      name        = "Runbook4-Weekly3"
+      frequency   = "Week"
+      interval    = 1
+      start_time  = "2024-09-19T01:00:00+10:00"
+      description = "Runbook4-Weekly3"
+      week_days   = ["Tuesday", "Friday"]
+      run_on      = ""
+    }
+  ]
+
+  automation_variables = [
+    {
+      name      = "Runbook4-Environment"
+      value     = "Production"
+      type      = "string"
+      encrypted = false
+    }
+  ]
+
+  tags = {
+    "Environment" = "Production"
+    "ManagedBy"   = "Terraform"
+    "Project"     = "Automation"
+  }
+}
---
+++ azure-runbooks/Test-ExampleRunbook4.ps1
@@ -0,0 +1,2 @@
+Write-Output "test-runbook-4"
+Write-Output "test-feature"
